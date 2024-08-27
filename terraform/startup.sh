b82e4bfcba8451fad60bb5e35213c6e54fb26dc0
asif <asif.khan@scikiq.com>
2024-08-27 11:22:38 +0000

    added voting app codes

---
+++ terraform/startup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Update package list
+sudo apt-get update
+
+# Install Jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y openjdk-11-jdk jenkins
+
+
+# Start Jenkins service
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+# Install Docker
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER && sudo usermod -aG docker jenkins
+ #&& newgrp docker
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install Helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# Print installation versions
+echo "Jenkins version:"
+jenkins --version
+echo "Docker version:"
+docker --version
+echo "kubectl version:"
+kubectl version --client
+echo "Helm version:"
+helm version
---
+++ terraform/var.tf
@@ -0,0 +1,31 @@
+# Declare variables
+variable "environment" {
+  default = "dev"
+}
+
+variable "prefix" {
+  default = "voting-app"
+}
+
+variable location {
+  default = "East US"
+}
+
+variable vm_size {
+  default = "Standard_B2s"
+}
+
+variable "virtualnetwork" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable subnet {
+  type    = list(string)
+  default = ["10.0.2.0/24"]
+}
+
+
+variable admin-password {
+  default = "Password1234!"
+}
